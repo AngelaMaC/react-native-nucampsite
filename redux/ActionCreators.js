4517767a93e16583ffa01964a838c62287e19234
Angela Canning <amc@angelacanning.com>
2020-02-18 13:33:32 -0600

    Swipeout and Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -148,9 +148,10 @@ export const postFavorite = campsiteId => dispatch => {
     }, 2000);
 };
 
-export const addFavorite = campsiteId => ({
-    type: ActionTypes.ADD_FAVORITE,
-    payload: campsiteId
+
+export const addComment = (comment) => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
 });
 
 export const postComment = (campsiteId, rating, author, text) => dispatch => {
@@ -166,7 +167,12 @@ export const postComment = (campsiteId, rating, author, text) => dispatch => {
     }, 2000);
 }
 
-export const addComment = (comment) => ({
-    type: ActionTypes.ADD_COMMENT,
-    payload: comment
+export const addFavorite = campsiteId => ({
+    type: ActionTypes.ADD_FAVORITE,
+    payload: campsiteId
 });
+
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+}); 
